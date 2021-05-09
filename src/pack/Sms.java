e0280f2a5918da9c1cd0daded22238a26c358be9
yonahersh <82897206+yonahersh@users.noreply.github.com>
2021-05-08 23:18:49 -0400

    Update Sms.java

--- src/pack/Sms.java
+++ src/pack/Sms.java
@@ -29,6 +29,61 @@ public class Sms implements App {
 				system.out.ln("Contact does not exist");
 		}
 	}	
+	
+	public void run(){
+		//Scanner scan1 = new Scanner(System.in);
+		MediaApp playlist = new MediaApp();
+		int a;
+
+		
+		while(true) 
+		{
+			Scanner scan = new Scanner(System.in);
+			try
+			{
+				System.out.println("what you you like to do?\r\n"
+					+"(1) Add a new chat\r\n"
+					+"(2) Erase a chat\r\n"
+					+"(3) Print a chat by contacts name\r\n"
+					+"(4) Search by sentence\r\n"	   
+					+"(5) Exit\n");
+			
+			a = scan.nextInt();// get users choice and call function accordingly 
+			scan.nextLine();// get rid of EOL symbol
+			
+			switch(a) 
+			{
+			case 1://add new media
+				playlist.add();
+				break;
+			case 2://play media by name
+				System.out.println("enter name of media\n");
+				String mName = scan.nextLine();
+				playlist.play(mName);
+				break;
+			case 3://play all media
+				playlist.playAll();
+				break;
+			case 4:
+				scan.close();
+				return;
+				
+			default:
+				System.out.println("choose an option between 1-4\n");
+			}//try
+			
+			}// switch case
+			catch (Exception e)
+			{
+				System.out.println(" please enter a valid input\n");
+			}
+			
+
+	}// while
+		
+	}//run
+
+	
 
 	void delete_chat(String name){		
 		int i = 0;
