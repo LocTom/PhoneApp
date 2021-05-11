2670df94a691cab0f976aba48cad6092c1ddbb60
yonahersh <82897206+yonahersh@users.noreply.github.com>
2021-05-11 10:30:24 -0400

    Update TestMobilePhone.java

--- src/pack/TestMobilePhone.java
+++ src/pack/TestMobilePhone.java
@@ -11,17 +11,23 @@ public class TestMobilePhone {
 		
 		while(true)
 		{
-			system.out.println("choose: 1,2 ,3,4");
+			system.out.println("choose: 1,2 ,3,4, (5)exit");
 			switch(a):
-			case(1):// phonebook
+			case 1:// phonebook
 				book.run();
 				break;
-			case(2):
-			
+			case 2:
+				calendar.run(
 				break;
 			case(3):
 			
 			case(4):
+			
+			case5:
+				return;
+			
+			default:
+				system.out.println("choose number 1-5");
 			
 			
 				
