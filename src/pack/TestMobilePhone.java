483561158f3d6c76689c4807cf52a8c8db64673e
yonahersh <82897206+yonahersh@users.noreply.github.com>
2021-05-11 10:48:54 -0400

    Update TestMobilePhone.java

--- src/pack/TestMobilePhone.java
+++ src/pack/TestMobilePhone.java
@@ -5,22 +5,34 @@ public class TestMobilePhone {
 	public static void main(String[] args) {
 		
 		PhoneBook book = new PhoneBook(); // use the constructor of ArrayList<contact>
-		Calendar calendar= new Calendar();
-		Sms sms = new Sms();
+		Calendar calendar= new Calendar(book);
+		Sms sms = new Sms(book);
 		Media media = new Media();
 		
 		while(true)
 		{
-			system.out.println("choose: 1,2 ,3,4, (5)exit");
+			Scanner scan = new Scanner(System.in);
+			try
+			{
+				System.out.println("what app you like to use?\r\n"
+					+"(1) Phonebook\r\n"
+					+"(2) Calander\r\n"
+					+"(3) Sms\r\n"
+					+"(4) Media\r\n"   
+					+"(5) Exit\n");
+			
+			a = scan.nextInt();// get users choice and call function accordingly 
+			scan.nextLine();// get rid of EOL symbol
+				
 			switch(a):
 			case 1:// phonebook
 				book.run();
 				break;
 			case 2:
-				calendar.run(book);
+				calendar.run();
 				break;
 			case(3):
-				sms.run(book);
+				sms.run();
 				retun;
 			case(4):
 				media.run();
@@ -30,7 +42,11 @@ public class TestMobilePhone {
 			
 			default:
 				system.out.println("choose number 1-5");
-			
+			}
+			catch (Exception e)
+			{
+				System.out.println("Please enter a valid input\n");
+			}
 			
 				
 			
